6ad7780508b9903d10f242c4178de29f106ffdb3
wannabesmith <iwaudbysmith@gmail.com>
2018-04-09 16:40:59 -0400

    * Add comments and a test case. * Now working

--- preliminaries/prelim.R
+++ preliminaries/prelim.R
@@ -3,20 +3,29 @@ gen.imp.resp <- function(df)
 {
   df.analysis <- df
   
-  for(q in names(df.analysis))
+  for(q in names(df.analysis)) # For each column
   {
+    # Construct regression formula with q as the response
     regression.formula <- as.formula(paste("as.ordered(", q, ")", "~ ."))
     inhs.polr <- polr(regression.formula, data = df.analysis, method = "probit")
     
-    sim.bins <- as.numeric(simulate(inhs.polr)[[1]])
+    sim.bins <- as.numeric(simulate(inhs.polr)[[1]]) # Simulate new bins
     
-    lower <- zeta[sim.bins]
-    upper <- zeta[sim.bins + 1]
+    lower <- zeta[sim.bins] # Get lower bounds of bins in sim.bins
+    upper <- zeta[sim.bins + 1] # Get upper bounds of bins in sim.bins
     
+    # Generate from truncated normal
     sim.truncnorm <- rtruncnorm(n = 1, a = lower, b = upper, mean = 0, sd = 1)
     
-    df.analysis[q] <- sim.truncnorm
+    df.analysis[q] <- sim.truncnorm # Update column
   }
   
-  df.analysis
+  return(df.analysis)
 }
+
+# Test 
+setwd("~/GitProjects/440proj") # Set to project folder
+multiis <- read.csv("data/MULTIIS.csv")
+imputed <- gen.imp.resp(multiis)
+
+head(imputed)
