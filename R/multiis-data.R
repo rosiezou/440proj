bb00715c534b8c6b81f6fff2f89a499626c2e2f0
wannabesmith <iwaudbysmith@gmail.com>
2018-04-17 11:12:34 -0400

    Add documentation for multiis. For example, run help(multiis)

---
+++ R/multiis-data.R
@@ -0,0 +1,21 @@
+#' Data of responses to The Multicultural Identity Integration Scale (MULTIIS)
+#'
+#' Data from the survey described in The Multicultural Identity Integration
+#' Scale (MULTIIS): Developing a Comprehensive Measure for Configuring One’s Multiple
+#' Cultural Identities Within the Self
+#'
+#' @docType data
+#'
+#' @usage data(multiis)
+#'
+#' @format A data.frame
+#'
+#' @keywords datasets
+#'
+#' @references Yampolsky et al. (2016) Cultural Diversity and Ethnic Minority Psychology http://dx.doi.org/10.1037/cdp0000043
+#'
+#' @examples
+#' data(multiis)
+#'
+#' head(multiis)
+"multiis"
