fec2bbc40b532fb2327db8702dbaaedd50881f74
rosiezou <rosiezou@gmail.com>
2018-04-20 14:52:20 -0400

    added 2D and 3D plots to the test script

--- test_script.R
+++ test_script.R
@@ -1,15 +1,27 @@
 library(stat440pkg)
-
 grp.indicator <- sapply(names(multiis), FUN =
                           function(x){strsplit(x, split = "_")[[1]][2]})
-
 latent.datasets <- gen.latent.datasets(100, multiis, grp.indicator = grp.indicator, num.iter = 5)
 pooled.add1 <- pool.analyses(latent.datasets, cat~comp + int, lm)
 pooled.add2 <- pool.analyses(latent.datasets, comp~cat + int, lm)
 pooled.add3 <- pool.analyses(latent.datasets, int~comp + cat, lm)
 
 
-library(Rcmdr)
+library(scatterplot3d)
 add <- function(x) Reduce("+", x)
 averaged <- add(latent.datasets)/100
-scatter3d(averaged$comp, averaged$cat, averaged$int)
+fit <- lm(int~comp + cat, data = averaged)
+scplot <- scatterplot3d(averaged$comp, averaged$cat, averaged$int, 
+              main="3D Scatterplot of Latent Variables", angle = 120,
+              xlab = "compartmentalization", ylab = "categorization", zlab = "integration", 
+              col.grid = "lightgrey", pch = 19, color = "lightblue")
+
+scplot$plane3d(fit, lty = "dotted")
+orig <- scplot$xyz.convert(averaged$comp, averaged$cat, averaged$int)
+plane <- scplot$xyz.convert(averaged$comp, averaged$cat, fitted(fit))
+i.negpos <- 1 + (resid(fit) > 0)
+segments(orig$x, orig$y, plane$x, plane$y,
+         col = c("blue", "red")[i.negpos], lty = (2:1)[i.negpos])
+
+pairs(averaged)
+plot(fit)
